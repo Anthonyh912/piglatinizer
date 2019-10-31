106c68c6e9f90b35370a17c6866818fdbdda4604
Anthony Hernandez <hernandez.aj912@gmail.com>
2019-10-31 21:53:38 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,18 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
     $("#button").click(function(){
-        console.log("hello")
-    })
+        let vowels = ["a","e","i","o","u"];
+        let words = $("input").val();
+        $(".output").text(words);
+        let letters = words.split("")
+        vowels.includes(letters[0])
+
+        if (vowels.includes(letters[0])) {
+            $(".output").append(word + "ay")
+        }
 
 
 
 
 
+    });
 });
